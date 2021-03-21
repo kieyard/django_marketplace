39e84710d1433c008bad45273736b641741b2dce
kieyard <kyardley@ymail.com>
2021-03-21 22:04:26 +0000

    created custom user model

---
+++ django_marketplace/accounts/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+	add_form = CustomUserCreationForm
+	form = CustomUserChangeForm
+	model = CustomUser
+	list_display = ('email', 'is_seller', 'is_buyer',)
+	list_filter = ('email', 'is_seller', 'is_buyer',)
+	fieldsets = (
+		(None, {'fields': ('email', 'password')}),
+		('Permissions', {'fields': ('is_staff', 'is_active', 'is_seller', 'is_buyer')}),
+	)
+
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide', ),
+			'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_seller', 'is_buyer')
+			}),
+		)
+	search_fields = ('email',)
+	ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ django_marketplace/accounts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+	email = models.EmailField(_('email address'), unique=True)
+	is_buyer = models.BooleanField(default=True)
+	is_seller = models.BooleanField(default=False)
+	is_staff = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+	date_joined = models.DateTimeField(default=timezone.now)
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = []
+
+	objects = CustomUserManager()
+
+	def __str__(self):
+		return self.email
+
